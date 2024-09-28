c7b5d336cc1fb43896c182eba6d0d49be06f0401
unknown <kemalcrisannnaufal@gmail.com>
2024-09-28 19:01:25 +0700

    add info blog

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,5 +1,7 @@
 const BlogsPage = () => {
-    return <div>BlogsPage</div>;
+    return <div className="flex justify-center h-[70vh] items-center">
+      <p className="text-md lg:text-xl text-center text-neutral-800 font-semibold">Alert | This Features is Under Development</p>
+    </div>;
   };
   
   export default BlogsPage;
